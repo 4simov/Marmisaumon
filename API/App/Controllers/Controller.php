4db4be34a4598b4553567df03946996edfe45356
4simov <corentin.pauline@outlook.com>
2024-05-13 14:41:41 +0200

    prise en compte du body dans les requêtes

--- API/App/Controllers/Controller.php
+++ API/App/Controllers/Controller.php
@@ -4,6 +4,9 @@ namespace Controllers;
 use System;
 use System\DatabaseConnector;
 
+/**
+ * Classe mère dont hériteront tous les controlleurs de l'application
+ */
 class Controller {
     public $db;
     public function __construct() {
--- API/App/Router.php
+++ API/App/Router.php
@@ -8,27 +8,32 @@ class Router {
         $this->routes = [];
     }
 
+    /**
+     * permet d'ajouter une association entre un url donné et une fonction d'un controlleur de l'application
+     * @param $route => l'url
+     * @param $controller => la classe qui doit être utilisée
+     * @param $action la fonction à appelé dans la classe
+     */
     public function addRoute($route, $controller, $action) {
         $this->routes[$route] = ['controller' => $controller, 'action' => $action];
     }
 
-    public function dispatch($uri, $request = null) {
+    public function dispatch($uri) {
         if (array_key_exists($uri, $this->routes)) {
             $controller = $this->routes[$uri]['controller'];
             $action = $this->routes[$uri]['action'];
 
             $controller = new $controller();
-            if($request != null) {
-                $controller->$action($request);
-                echo 'OPTION';
+            $requestBody = file_get_contents('php://input');
+
+            if($requestBody != null) {
+                $controller->$action($requestBody);
             }
             else {
                 $controller->$action();
             }
         } else {
             include __DIR__ .'/Errors/404.php';
-            //echo 'Error 404 : Tu es sur une page inexistante 0_0.';
-            //throw new \Exception("No route found for URI: $uri");
         }
     }
 }
