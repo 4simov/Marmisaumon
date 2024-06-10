5f4dc8d4933f3217998ece3934f48ae5ba51dc3e
4simov <corentin.pauline@outlook.com>
2024-06-10 11:49:50 +0200

    résolution CORS !

--- API/index.php
+++ API/index.php
@@ -1,16 +1,4 @@
 <?php
-
-use App\Routes;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\ServerRequest;
-
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/App/Routes.php';
-
-$uri = $_SERVER['REQUEST_URI'];
-<?php
-// Ajoutez ceci en haut de votre fichier index.php ou de votre contrôleur principal
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
@@ -20,7 +8,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+use App\Routes;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
 
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/App/Routes.php';
+
+$uri = $_SERVER['REQUEST_URI']
 // Le reste de votre code
 $request = ServerRequest::fromGlobals();
 $response = new Response();
