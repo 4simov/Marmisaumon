437748c55e4af8591eb8e0209d453eccb4804b79
4simov <corentin.pauline@outlook.com>
2024-06-11 16:55:31 +0200

    correctif RoleEnum

--- API/App/Enums/RolesEnum.php
+++ API/App/Enums/RolesEnum.php
@@ -3,9 +3,10 @@ namespace MyEnum{
 
 enum RolesEnum : int
 {
-    case INVITE = 0;
-    case UTILISATEUR = 1;
-    case ADMIN = 2;
+    case INVITE = 1;
+    case UTILISATEUR = 2;
+    case MODERATEUR = 3;
+    case ADMIN = 4;
 }
 
 }
