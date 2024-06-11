510f6582fa539f8b641f5eeac560ce418600e035
4simov <corentin.pauline@outlook.com>
2024-06-11 16:48:04 +0200

    finalisation inscription connexion + retouche role header

--- flutter/lib/Utils/constants.dart
+++ flutter/lib/Utils/constants.dart
@@ -1,2 +1,2 @@
-//const String API_URL="localhost:8080";//local
-const String API_URL="217.182.175.145:8000";//Remote
+const String API_URL="localhost:8080";//local
+//const String API_URL="217.182.175.145:8000";//Remote
