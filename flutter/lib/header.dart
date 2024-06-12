6a7d402b7989cfe51a7aa371ea546da8fefb3d3f
Lilou <lilouborey@gmail.com>
2024-06-12 09:09:52 +0200

    ajout page de deconnexion

--- flutter/lib/header.dart
+++ flutter/lib/header.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
-// ignore: unnecessary_import
-import 'package:flutter/rendering.dart';
 import 'package:marmisaumon/Enums/roleEnum.dart';
+import 'Utils/cookieManager.dart';
 
 class HeaderWidget extends StatefulWidget {
   const HeaderWidget({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _HeaderWidgetState createState() => _HeaderWidgetState();
 }
 
 class _HeaderWidgetState extends State<HeaderWidget> {
   String _hoveredItem = '';
   RoleEnum role = RoleEnum.UTILISATEUR;
+  final CookieManager _cookieManager = CookieManager();
+
+  Future<void> _showLogoutConfirmation(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button for close dialog!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmation'),
+          content: const Text('Voulez-vous vraiment vous déconnecter ?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Déconnexion'),
+              onPressed: () async {
+                await _cookieManager.deleteCookie('token');
+                Navigator.of(context).pop();
+                Navigator.pushNamedAndRemoveUntil(context, '/connexion', (Route<dynamic> route) => false);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,20 +61,22 @@ class _HeaderWidgetState extends State<HeaderWidget> {
           const SizedBox(height: 20.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
+            children: <Widget>[
               _buildMenuItem(context, 'Accueil', '/'),
               _buildMenuItem(context, 'Recettes', '/affichage-recette'),
-              if(role.value >= RoleEnum.UTILISATEUR.value )
-                _buildMenuItem(context, 'Créer une recette', '/creation-recette'),//montrer que pour invité
-              if(role.value >= RoleEnum.UTILISATEUR.value )
-                _buildMenuItem(context, 'Mon compte', '/profil'),//montrer que pour utilisateur pour utilisateur 
+              if (role.value >= RoleEnum.UTILISATEUR.value)
+                _buildMenuItem(context, 'Créer une recette', '/creation-recette'),
+              if (role.value >= RoleEnum.UTILISATEUR.value)
+                _buildMenuItem(context, 'Mon compte', '/profil'),
               _buildMenuItem(context, 'Contact', '/contact'),
-              if(role.value >= RoleEnum.UTILISATEUR.value )
-                _buildMenuItem(context, 'Nouvel ingrédient', 'ajoutIngredient'),//montrer que pour Utilisateur ou plus
-              if(role.value >= RoleEnum.ADMIN.value )
-                _buildMenuItem(context, 'Admin', '/admin'),//montrer que pour l'admin
-              if(role.value < RoleEnum.UTILISATEUR.value)
+              if (role.value >= RoleEnum.UTILISATEUR.value)
+                _buildMenuItem(context, 'Nouvel ingrédient', 'ajoutIngredient'),
+              if (role.value >= RoleEnum.ADMIN.value)
+                _buildMenuItem(context, 'Admin', '/admin'),
+              if (role.value < RoleEnum.UTILISATEUR.value)
                 _buildMenuItem(context, 'Inscription - Connexion', '/connexion'),
+              if (role.value >= RoleEnum.UTILISATEUR.value)
+                _buildMenuItem(context, 'Déconnexion', '', isLogout: true),
             ],
           ),
         ],
@@ -53,7 +84,7 @@ class _HeaderWidgetState extends State<HeaderWidget> {
     );
   }
 
-  Widget _buildMenuItem(BuildContext context, String title, String route) {
+  Widget _buildMenuItem(BuildContext context, String title, String route, {bool isLogout = false}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: MouseRegion(
@@ -69,7 +100,11 @@ class _HeaderWidgetState extends State<HeaderWidget> {
         },
         child: InkWell(
           onTap: () {
-            Navigator.pushNamed(context, route);
+            if (isLogout) {
+              _showLogoutConfirmation(context);
+            } else {
+              Navigator.pushNamed(context, route);
+            }
           },
           child: Text(
             title,
